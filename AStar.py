b346c1c5a342f9d296302388a63477b9bdd6664a
Sherif66 <67288812+Sherif66@users.noreply.github.com>
2022-04-23 15:14:03 +0200

    Update AStar.py

--- AStar.py
+++ AStar.py
@@ -8,13 +8,13 @@ def heuristicCost(cell1,cell2):
 def aStar(inputMaze):
     startCell=(m.rows,m.cols)
     goalCell=(1, 1)
-    g_score={cell: float("inf") for cell in m.grid}
-    g_score[startCell]=0
-    f_score = {cell: float("inf") for cell in m.grid}
-    f_score[startCell]=heuristicCost(startCell, goalCell)
+    h_score={cell: float("inf") for cell in m.grid}
+    h_score[startCell]=0
+    h2_score = {cell: float("inf") for cell in m.grid}
+    h2_score[startCell]=heuristicCost(startCell, goalCell)
 
     cellPriorityQueue=PriorityQueue()
-    cellPriorityQueue.put((heuristicCost(startCell, goalCell) + g_score[startCell], heuristicCost(startCell, goalCell), startCell))
+    cellPriorityQueue.put((heuristicCost(startCell, goalCell) + h_score[startCell], heuristicCost(startCell, goalCell), startCell))
     pathWithReservedArrows = {}
     searchPath=[startCell]
     while not cellPriorityQueue.empty():
@@ -25,20 +25,20 @@ def aStar(inputMaze):
         for direction in 'ESNW':
             if m.maze_map[currCell][direction]==True:
                 if direction == 'E':
-                    childCell=(currCell[0],currCell[1]+1)
+                    child =(currCell[0],currCell[1]+1)
                 elif direction == 'S':
-                    childCell = (currCell[0]+1, currCell[1] )
+                    child  = (currCell[0]+1, currCell[1] )
                 elif direction == 'N':
-                    childCell = (currCell[0]-1, currCell[1] )
+                    child = (currCell[0]-1, currCell[1] )
                 elif direction == 'W':
-                    childCell = (currCell[0], currCell[1]-1 )
+                    child = (currCell[0], currCell[1]-1 )
 
-                tempGScore= g_score[currCell] + 1
-                tempFScore= tempGScore + heuristicCost(childCell, goalCell)
+                tempGScore= h_score[currCell] + 1
+                tempFScore= tempGScore + heuristicCost(child, goalCell)
 
-                if tempFScore < f_score[childCell]:
-                 g_score[childCell] = tempGScore
-                 f_score[childCell] = tempFScore
+                if tempFScore < h2_score[childCell]:
+                 h_score[childCell] = tempGScore
+                 h2_score[childCell] = tempFScore
                  cellPriorityQueue.put((tempFScore, heuristicCost(childCell, goalCell), childCell))
                  pathWithReservedArrows[childCell] = currCell
 
@@ -50,11 +50,12 @@ def aStar(inputMaze):
     return searchPath,path
 
 
-m=maze(15,15)
-m.CreateMaze(theme='light')
+m=maze(10,10)
+m.CreateMaze(theme='purple')
 searchPath, aPath= aStar(m)
-a = agent(m, footprints=True, color=COLOR.blue, filled=True)
+a = agent(m, footprints=True, color=COLOR.maroon, filled=True)
 b = agent(m, footprints=True, color=COLOR.yellow, filled=True, goal=(m.rows, m.cols))
+
 
 
 m.tracePath({a: searchPath}, delay=300)
@@ -63,3 +64,4 @@ m.tracePath({b: aPath}, delay=300)
 l = textLabel(m, 'A Star Path Length', len(aPath) + 1)
 l = textLabel(m, 'A Star Search Length', len(searchPath))
 m.run()
+
